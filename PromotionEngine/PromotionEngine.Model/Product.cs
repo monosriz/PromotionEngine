3c863a933b6527faae8de12a022444f917e19020
Monosriz Dutta <monosrizms@gmail.com>
2021-03-13 07:19:35 +0100

    Product model

---
+++ PromotionEngine/PromotionEngine.Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Model
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+
+
+        public Product(String ID, decimal Price)
+        {
+            this.Id = ID;
+            this.Price = Price;
+        }
+    }
+}
